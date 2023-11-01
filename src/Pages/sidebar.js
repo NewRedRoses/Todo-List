6f506a41d0870384c14742ba74a2dc2a2b951ac6
Mark A. <mark001616@gmail.com>
2023-11-01 18:04:03 -0400

    Add sidebar UI with components

---
+++ src/Pages/sidebar.js
@@ -0,0 +1,32 @@
+export default function sidebar() {
+  // Tasks section
+  const sidebarTasksContainer = document.querySelector(".task-options");
+  const tasksTitle = document.createElement("div");
+  tasksTitle.classList.add("sidebar-title");
+  tasksTitle.textContent = "Tasks";
+  sidebarTasksContainer.appendChild(tasksTitle);
+
+  const taskButtons = ["All", "Today", "Tomorrow"];
+  taskButtons.forEach((title) => {
+    const button = document.createElement("button");
+    button.classList.add("task-filter-btn");
+    button.textContent = title;
+    sidebarTasksContainer.appendChild(button);
+  });
+
+  // Tags section
+  const sidebarTagsContainer = document.querySelector(".tags-options");
+  const tagsTitle = document.createElement("div");
+  tagsTitle.classList.add("sidebar-title");
+
+  tagsTitle.textContent = "Tags";
+  sidebarTagsContainer.appendChild(tagsTitle);
+
+  const tagButtons = ["School", "Work", "Random"];
+  tagButtons.forEach((title) => {
+    const button = document.createElement("button");
+    button.classList.add("task-filter-btn");
+    button.textContent = title;
+    sidebarTagsContainer.appendChild(button);
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { Task, TaskManager } from "./Task";
 import header from "./Pages/header";
 import "./style.css";
+import sidebar from "./Pages/sidebar";
 
 let listOfTasks = [];
 
 header();
+sidebar();
