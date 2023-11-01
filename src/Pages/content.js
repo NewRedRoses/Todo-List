e4ad86a51c8e931fb3f23a88ace84f0fcff989d7
Mark A. <mark001616@gmail.com>
2023-11-01 18:35:34 -0400

    Add content component

---
+++ src/Pages/content.js
@@ -0,0 +1,6 @@
+export default function content() {
+  const container = document.querySelector(".content");
+  const sampleText = document.createElement("div");
+  sampleText.textContent = "Testing container";
+  container.appendChild(sampleText);
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { Task, TaskManager } from "./Task";
 import header from "./Pages/header";
-import "./style.css";
 import sidebar from "./Pages/sidebar";
+import content from "./Pages/content";
+import "./style.css";
 
 let listOfTasks = [];
 
 header();
 sidebar();
+content();
