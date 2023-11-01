01b8b9b2631519103b625a6edccb01a9f2108bbc
Mark A. <mark001616@gmail.com>
2023-11-01 02:19:35 -0400

    Add sample task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("webpack");
+import { Task } from "./Task";
+import headerPage from "./headerPage";
+import "./style.css";
+
+const container = document.createElement("div");
+container.classList.add("container");
+
+headerPage(container);
+const test = new Task("Do hw", "finish bio and chem hw");
+console.log(test);
+
+const body = document.querySelector("body");
+body.appendChild(container);
