1727757ec6ceae31ebba22a539aedb201544b8e5
Mark A. <mark001616@gmail.com>
2023-10-31 23:13:53 -0400

    Create Task class

---
+++ src/Task.js
@@ -0,0 +1,11 @@
+class Task {
+  constructor(name, description, dueDate, priority, checklist) {
+    this.taskName = name;
+    this.taskDescription = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checklist = checklist;
+  }
+}
+
+export { Task };
