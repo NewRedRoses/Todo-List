b80effad500313466e33950cfdeb080f8bc2729c
Mark A. <mark001616@gmail.com>
2023-11-02 19:29:47 -0400

    Add create task btn

--- src/Pages/header.js
+++ src/Pages/header.js
@@ -1,7 +1,14 @@
+import { createTaskModal, showTaskModal } from "/src/Modal.js";
 export default function headerPage() {
   const header = document.querySelector(".header");
 
   const title = document.createElement("div");
   title.textContent = "YATA - Yet Another Todo App";
   header.appendChild(title);
+  // Create task/ project button
+
+  const createTaskBtn = document.createElement("button");
+  createTaskBtn.textContent = "+";
+  createTaskBtn.classList.add("add-task-btn");
+  header.appendChild(createTaskBtn);
 }
