d4874689d8fca5e31d4820e05197413e41d35ce3
Mark A. <mark001616@gmail.com>
2023-11-04 00:47:00 -0400

    Refactor event listeners into function

--- src/Modal.js
+++ src/Modal.js
@@ -1,15 +1,32 @@
 function showTaskModal() {
   const dialog = document.querySelector("dialog");
+
+  showButtonEventListener(dialog);
+  closeButtonEventListener(dialog);
+  submitButtonEventListener(dialog);
+}
+
+function showButtonEventListener(dialog) {
   const showButton = document.querySelector(".add-task-btn");
-  const closeButton = document.querySelector("dialog button");
 
   // "Show the dialog" button opens the dialog modally
   showButton.addEventListener("click", () => {
     dialog.showModal();
   });
+}
 
+function closeButtonEventListener(dialog) {
+  const closeButton = document.querySelector("dialog button");
   // "Close" button closes the dialog
   closeButton.addEventListener("click", () => {
+    dialog.close();
+  });
+}
+
+function submitButtonEventListener(dialog) {
+  const submitBtn = document.querySelector(".submit-task-btn");
+
+  submitBtn.addEventListener("click", () => {
     dialog.close();
   });
 }
