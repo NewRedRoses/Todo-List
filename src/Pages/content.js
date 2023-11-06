7f4e9761443aba97bb350139e31c0063a39ad23a
Mark A. <mark001616@gmail.com>
2023-11-06 14:33:18 -0500

    Remove sample text

--- src/Pages/content.js
+++ src/Pages/content.js
@@ -1,6 +1,6 @@
 export default function content() {
   const container = document.querySelector(".content");
   const sampleText = document.createElement("div");
-  sampleText.textContent = "Testing container";
+  //   sampleText.textContent = "Testing container";
   container.appendChild(sampleText);
 }
