8d8d751f3e6c5d5dbf5ef86983f824645118c352
Mark A. <mark001616@gmail.com>
2023-11-08 16:54:43 -0500

    Reorganize demoTasks function & others

--- src/Pages/content.js
+++ src/Pages/content.js
@@ -3,33 +3,31 @@ import { TaskDisplayer, TaskCreator, TaskManager } from "../Task";
 export default function content() {
   const container = document.querySelector(".content");
   const sampleText = document.createElement("div");
-  
-  let listOfTasks = [];
-  
-  // Dummy samples to showcase cards + tasks
-  function demoTasks() {
-    const sampleTasks = [
-      "Do Homework",
-      "Wash car",
-      "Go to Protest",
-      "Read book",
-      "Finish coding course",
-      "Do laundry",
-    ];
-    sampleTasks.forEach((sampleTask) => {
-      let task = TaskCreator(
-        sampleTask,
-        "sample description",
-        new Date(),
-        undefined,
-        listOfTasks
-      );
-      TaskManager.addThisTaskToArray(task, listOfTasks);
-    });
-    TaskDisplayer.displayAllTasks(listOfTasks);
-  }
-  console.log(listOfTasks);
-  demoTasks();
-
   container.appendChild(sampleText);
+
+  let listOfTasks = [];
+  demoTasks(listOfTasks);
+}
+
+// Dummy samples to showcase cards + tasks
+function demoTasks(listOfTasks) {
+  const sampleTasks = [
+    "Do Homework",
+    "Wash car",
+    "Go to Protest",
+    "Read book",
+    "Finish coding course",
+    "Do laundry",
+  ];
+  sampleTasks.forEach((sampleTask) => {
+    let task = TaskCreator(
+      sampleTask,
+      "sample description",
+      new Date(),
+      undefined,
+      listOfTasks
+    );
+    TaskManager.addThisTaskToArray(task, listOfTasks);
+  });
+  TaskDisplayer.displayAllTasks(listOfTasks);
 }
