591c578d8b473403c4e6c7cfd5b99af92eecec5c
Mark A. <mark001616@gmail.com>
2023-11-08 16:44:57 -0500

    Move sample task from index to content file

--- src/Pages/content.js
+++ src/Pages/content.js
@@ -1,6 +1,35 @@
+import { TaskDisplayer, TaskCreator, TaskManager } from "../Task";
+
 export default function content() {
   const container = document.querySelector(".content");
   const sampleText = document.createElement("div");
-  //   sampleText.textContent = "Testing container";
+  
+  let listOfTasks = [];
+  
+  // Dummy samples to showcase cards + tasks
+  function demoTasks() {
+    const sampleTasks = [
+      "Do Homework",
+      "Wash car",
+      "Go to Protest",
+      "Read book",
+      "Finish coding course",
+      "Do laundry",
+    ];
+    sampleTasks.forEach((sampleTask) => {
+      let task = TaskCreator(
+        sampleTask,
+        "sample description",
+        new Date(),
+        undefined,
+        listOfTasks
+      );
+      TaskManager.addThisTaskToArray(task, listOfTasks);
+    });
+    TaskDisplayer.displayAllTasks(listOfTasks);
+  }
+  console.log(listOfTasks);
+  demoTasks();
+
   container.appendChild(sampleText);
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
-import { TaskDisplayer, TaskCreator, TaskManager } from "./Task";
 import header from "./Pages/header";
 import sidebar from "./Pages/sidebar";
 import content from "./Pages/content";
 import "./style.css";
-import { Card } from "./Card";
 
-let listOfTasks = [];
 
 header();
 sidebar();
 content();
 
-// Dummy samples to showcase cards + tasks
-function demoTasks() {
-  const sampleTasks = [
-    "Do Homework",
-    "Wash car",
-    "Go to Protest",
-    "Read book",
-    "Finish coding course",
-    "Do laundry",
-  ];
-  sampleTasks.forEach((sampleTask) => {
-    let task = TaskCreator(sampleTask, "sample description", new Date());
-    TaskManager.addThisTaskToArray(task, listOfTasks);
-  });
-  TaskDisplayer.displayAllTasks(listOfTasks);
-}
-console.log(listOfTasks);
-demoTasks();
