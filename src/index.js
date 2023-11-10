de5b94be159cf23c82d3c00b2308687f9e45cb72
Mark A. <mark001616@gmail.com>
2023-11-10 14:07:24 -0500

    Use listOfTasks  array globally

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import sidebar from "./Pages/sidebar";
 import content from "./Pages/content";
 import "./style.css";
 
-
+// Array where all tasks go
+export let listOfTasks = [];
 header();
 sidebar();
 content();
-
