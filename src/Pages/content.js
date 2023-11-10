e840396c80f1b39f0730e7f4c8a963e329e0876e
Mark A. <mark001616@gmail.com>
2023-11-10 14:16:44 -0500

    Display cards based on the task btn pressed

--- src/Pages/content.js
+++ src/Pages/content.js
@@ -1,33 +1,40 @@
-import { TaskDisplayer, TaskCreator, TaskManager } from "../Task";
+import { TaskDisplayer } from "../Task";
+import { listOfTasks } from "..";
+import { removeAllCards } from "../Card";
 
-export default function content() {
+export default function content(sectionToRender) {
   const container = document.querySelector(".content");
   const sampleText = document.createElement("div");
   container.appendChild(sampleText);
+  console.log("Section to render: ", sectionToRender);
+  // To be removed
+  // demoTasks();
+  console.log(listOfTasks);
+  const date = new Date();
+  const today = date.getDate();
+  const tomorrow = today + 1; // expect for days when end of month
 
-  let listOfTasks = [];
-  demoTasks(listOfTasks);
-}
-
-// Dummy samples to showcase cards + tasks
-function demoTasks(listOfTasks) {
-  const sampleTasks = [
-    "Do Homework",
-    "Wash car",
-    "Go to Protest",
-    "Read book",
-    "Finish coding course",
-    "Do laundry",
-  ];
-  sampleTasks.forEach((sampleTask) => {
-    let task = TaskCreator(
-      sampleTask,
-      "sample description",
-      "",
-      undefined,
-      listOfTasks
-    );
-    TaskManager.addThisTaskToArray(task, listOfTasks);
-  });
-  TaskDisplayer.displayAllTasks(listOfTasks);
+  switch (sectionToRender) {
+    case "all":
+      TaskDisplayer.displayAllTasks(listOfTasks);
+      break;
+    case "today":
+      // In theory would need to be more robust than just today's digit
+      // It would need to have the same day, month and year.
+      console.log("testing the today, with value: ", today);
+      const listOfTodayTasks = listOfTasks.filter((task) => {
+        return task.dueDate.getDate() == today;
+      });
+      console.log(listOfTodayTasks);
+      TaskDisplayer.displayAllTasks(listOfTodayTasks);
+      break;
+    case "tomorrow":
+      console.log("testing the today, with value: ", tomorrow);
+      const listOfTomorrowTasks = listOfTasks.filter((task) => {
+        return task.dueDate.getDate() == tomorrow;
+      });
+      console.log(listOfTomorrowTasks);
+      TaskDisplayer.displayAllTasks(listOfTomorrowTasks);
+      break;
+  }
 }
