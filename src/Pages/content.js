6ba913a641b2f359e2c150ec595711d8603b3f6e
Mark A. <mark001616@gmail.com>
2023-11-10 20:39:59 -0500

    Remove unnecessary lines and file

--- src/Pages/content.js
+++ src/Pages/content.js
@@ -1,12 +1,10 @@
 import { TaskDisplayer } from "../Task";
 import { listOfTasks } from "..";
-import { removeAllCards } from "../Card";
 
 export default function content(sectionToRender) {
   const container = document.querySelector(".content");
   const sampleText = document.createElement("div");
   container.appendChild(sampleText);
-  console.log("Section to render: ", sectionToRender);
 
   // Handle today
   let date = new Date();
@@ -22,19 +20,15 @@ export default function content(sectionToRender) {
     case "today":
       // In theory would need to be more robust than just today's digit
       // It would need to have the same day, month and year.
-      console.log("testing the today, with value: ", today);
       const listOfTodayTasks = listOfTasks.filter((task) => {
         return task.dueDate.getDate() == today;
       });
-      console.log(listOfTodayTasks);
       TaskDisplayer.displayAllTasks(listOfTodayTasks);
       break;
     case "tomorrow":
-      console.log("testing the today, with value: ", tomorrow);
       const listOfTomorrowTasks = listOfTasks.filter((task) => {
         return task.dueDate.getDate() == tomorrow;
       });
-      console.log(listOfTomorrowTasks);
       TaskDisplayer.displayAllTasks(listOfTomorrowTasks);
       break;
   }
