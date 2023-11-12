ce268213d96dd3ef0be88209cac63baa55542d25
Mark A. <mark001616@gmail.com>
2023-11-11 22:07:06 -0500

    Add ESLint fixes

--- src/Card.js
+++ src/Card.js
@@ -1,77 +1,77 @@
-import { formatDistanceToNow, isPast } from "date-fns";
-import { TaskManager } from "./Task";
-import { listOfTasks } from ".";
-import { setCardPriority } from "./Priority";
+import { formatDistanceToNow, isPast } from 'date-fns'
+import { TaskManager } from './Task'
+import { listOfTasks } from '.'
+import { setCardPriority } from './Priority'
 class Card {
-  static deleteButtonCounter = 0;
-  constructor(task) {
-    this.task = task;
-    this.cardContainer = document.createElement("div");
-    this.cardContainer.classList.add("card-container");
+  static deleteButtonCounter = 0
+  constructor (task) {
+    this.task = task
+    this.cardContainer = document.createElement('div')
+    this.cardContainer.classList.add('card-container')
 
-    const topSection = document.createElement("div");
-    topSection.classList.add("card-top");
+    const topSection = document.createElement('div')
+    topSection.classList.add('card-top')
     // Title and delete button section
-    const topProperties = ["title", "delete"];
+    const topProperties = ['title', 'delete']
     topProperties.forEach((property) => {
-      if (property == "title") {
-        this.item = document.createElement("div");
-        this.item.textContent = task[property];
+      if (property === 'title') {
+        this.item = document.createElement('div')
+        this.item.textContent = task[property]
       } else {
-        this.item = document.createElement("button");
-        this.item.textContent = "X";
-        this.item.id = `deleteBtn-${Card.deleteButtonCounter}`;
-        Card.deleteButtonCounter++;
+        this.item = document.createElement('button')
+        this.item.textContent = 'X'
+        this.item.id = `deleteBtn-${Card.deleteButtonCounter}`
+        Card.deleteButtonCounter++
       }
-      this.item.classList.add(`card-${property}`);
-      topSection.appendChild(this.item);
-    });
-    this.cardContainer.appendChild(topSection);
+      this.item.classList.add(`card-${property}`)
+      topSection.appendChild(this.item)
+    })
+    this.cardContainer.appendChild(topSection)
 
     // Description and due date section
-    const bottomSection = document.createElement("div");
-    bottomSection.classList.add("card-bottom");
+    const bottomSection = document.createElement('div')
+    bottomSection.classList.add('card-bottom')
 
-    const bottomProperties = ["description", "dueDate"];
+    const bottomProperties = ['description', 'dueDate']
     bottomProperties.forEach((property) => {
-      this.item = document.createElement("div");
-      this.item.classList.add(`card-${property}`);
-      if (property == "dueDate") {
-        const taskDate = task[property];
+      this.item = document.createElement('div')
+      this.item.classList.add(`card-${property}`)
+      if (property === 'dueDate') {
+        const taskDate = task[property]
         if (isPast(task[property])) {
           this.item.textContent =
-            "Was due " + formatDistanceToNow(taskDate) + " ago";
+            'Was due ' + formatDistanceToNow(taskDate) + ' ago'
         } else {
-          this.item.textContent = "Due in " + formatDistanceToNow(taskDate);
+          this.item.textContent = 'Due in ' + formatDistanceToNow(taskDate)
         }
       } else {
-        this.item.textContent = task[property];
+        this.item.textContent = task[property]
       }
-      bottomSection.appendChild(this.item);
-    });
-    this.cardContainer.appendChild(bottomSection);
+      bottomSection.appendChild(this.item)
+    })
+    this.cardContainer.appendChild(bottomSection)
 
     // Add the card container to the main container
-    this.container = document.querySelector(".cards");
-    this.container.appendChild(this.cardContainer);
+    this.container = document.querySelector('.cards')
+    this.container.appendChild(this.cardContainer)
 
     // Event listener section
     this.deleteBtn = document.getElementById(
       `deleteBtn-${Card.deleteButtonCounter - 1}`
-    );
-    this.deleteBtn.addEventListener("click", this.deleteCard.bind(this));
-    setCardPriority(this.cardContainer, this.task.priority);
+    )
+    this.deleteBtn.addEventListener('click', this.deleteCard.bind(this))
+    setCardPriority(this.cardContainer, this.task.priority)
   }
 
-  deleteCard() {
-    TaskManager.removeThisTaskFromArray(this.task, listOfTasks);
-    this.cardContainer.remove();
+  deleteCard () {
+    TaskManager.removeThisTaskFromArray(this.task, listOfTasks)
+    this.cardContainer.remove()
   }
 }
 
-function removeAllCards() {
-  const container = document.querySelector(".content");
-  container.innerHTML = "";
+function removeAllCards () {
+  const container = document.querySelector('.content')
+  container.innerHTML = ''
 }
 
-export { Card, removeAllCards };
+export { Card, removeAllCards }
--- src/Modal.js
+++ src/Modal.js
@@ -1,63 +1,63 @@
-import { TaskCreator, TaskDisplayer, TaskManager } from "./Task";
-import { listOfTasks } from ".";
-function showTaskModal() {
-  const dialog = document.querySelector('dialog');
-  eventListenerHandler.showButtonEventListener(dialog);
-  eventListenerHandler.closeButtonEventListener(dialog);
-  eventListenerHandler.taskFormValuesEventListener(dialog);
-  eventListenerHandler.submitButtonEventListener(dialog);
+import { TaskCreator, TaskDisplayer, TaskManager } from './Task'
+import { listOfTasks } from '.'
+function showTaskModal () {
+  const dialog = document.querySelector('dialog')
+  eventListenerHandler.showButtonEventListener(dialog)
+  eventListenerHandler.closeButtonEventListener(dialog)
+  eventListenerHandler.taskFormValuesEventListener(dialog)
+  eventListenerHandler.submitButtonEventListener(dialog)
 }
 
 const eventListenerHandler = (() => {
   const showButtonEventListener = (dialog) => {
-    const showButton = document.querySelector(".add-task-btn");
+    const showButton = document.querySelector('.add-task-btn')
     // "Show the dialog" button opens the dialog modally
-    showButton.addEventListener("click", () => {
-      dialog.showModal();
-    });
-  };
+    showButton.addEventListener('click', () => {
+      dialog.showModal()
+    })
+  }
 
   const closeButtonEventListener = (dialog) => {
-    const closeButton = document.querySelector("dialog button");
+    const closeButton = document.querySelector('dialog button')
     // "Close" button closes the dialog
-    closeButton.addEventListener("click", () => {
-      dialog.close();
-    });
-  };
+    closeButton.addEventListener('click', () => {
+      dialog.close()
+    })
+  }
 
   const submitButtonEventListener = (dialog) => {
-    const submitBtn = document.querySelector(".submit-task-btn");
-    submitBtn.addEventListener("click", () => {});
-  };
+    const submitBtn = document.querySelector('.submit-task-btn')
+    submitBtn.addEventListener('click', () => {})
+  }
   const taskFormValuesEventListener = (dialog) => {
-    const form = document.querySelector(".task-creation-form");
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      let taskProperties = [];
-      dialog.close();
-      const formData = new FormData(form);
+    const form = document.querySelector('.task-creation-form')
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      const taskProperties = []
+      dialog.close()
+      const formData = new FormData(form)
       for (const pair of formData.entries()) {
-        taskProperties.push(pair[1]);
+        taskProperties.push(pair[1])
       }
-      let task = TaskCreator(
+      const task = TaskCreator(
         taskProperties[0],
         taskProperties[1],
         taskProperties[2],
         taskProperties[3],
         taskProperties[4]
-      );
-      TaskManager.addThisTaskToArray(task, listOfTasks);
-      TaskDisplayer.displayTask(task);
-      form.reset();
-    });
-  };
+      )
+      TaskManager.addThisTaskToArray(task, listOfTasks)
+      TaskDisplayer.displayTask(task)
+      form.reset()
+    })
+  }
 
   return {
     showButtonEventListener,
     closeButtonEventListener,
     submitButtonEventListener,
-    taskFormValuesEventListener,
-  };
+    taskFormValuesEventListener
+  }
 })()
 
-export { showTaskModal };
+export { showTaskModal }
